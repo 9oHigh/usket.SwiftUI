8f496eee5dacf80edbee8bbed7e07864f60b32bd
9oHigh <wkdska957@naver.com>
2021-08-16 20:37:18 +0900

    Swift Basic

---
+++ SwiftUIBasic/SwiftUIBasic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBasic
+//
+//  Created by 이경후 on 2021/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

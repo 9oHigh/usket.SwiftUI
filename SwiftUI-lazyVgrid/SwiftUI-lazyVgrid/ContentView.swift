c2a254994acfc3ee4549b7b041c8a73fa803cea0
9oHigh <wkdska957@naver.com>
2022-01-02 22:38:44 +0900

    Organize

---
+++ SwiftUI-lazyVgrid/SwiftUI-lazyVgrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-lazyVgrid
+//
+//  Created by 이경후 on 2021/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-lazyVgrid/SwiftUI-lazyVgrid/SwiftUI_lazyVgridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_lazyVgridApp.swift
+//  SwiftUI-lazyVgrid
+//
+//  Created by 이경후 on 2021/09/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_lazyVgridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
